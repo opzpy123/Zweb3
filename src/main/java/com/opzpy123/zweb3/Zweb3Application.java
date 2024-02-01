3cdb3744118061844328ba31afc5d704ed4333b1
赵鹏毅 <zhaopy@gantsoftware.com>
2024-02-01 11:05:28 +0800

    CommandLineRunner用例

--- src/main/java/com/opzpy123/zweb3/Zweb3Application.java
+++ src/main/java/com/opzpy123/zweb3/Zweb3Application.java
@@ -1,8 +1,12 @@
 package com.opzpy123.zweb3;
 
+import com.opzpy123.zweb3.component.kafka.conf.KafkaConf;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @MapperScan("com.opzpy123.zweb3.web.mapper")
@@ -12,4 +16,11 @@ public class Zweb3Application {
         SpringApplication.run(Zweb3Application.class, args);
     }
 
+    //启动时自动执行 参数可以是任何bean
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, Object> kafkaTemplate) {
+        return args -> {
+            kafkaTemplate.send(KafkaConf.topic, "hello kafka");
+        };
+    }
 }
